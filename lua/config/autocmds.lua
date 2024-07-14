71254097e9a25ecb69f0f7eacc871ef96775ee83
ilaumjd <ilaumjd@gmail.com>
2024-07-14 16:11:05 +0700

    default extras

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+------ DISABLE HIGHLGHT AFTER SEARCH ------
 vim.cmd([[
   augroup disable_highlight_after_search
     autocmd!
     autocmd CursorMoved * set nohlsearch
   augroup END
 ]])
+
+------ DISABLE DEFAULT KEYMAP ------
+vim.cmd("map q <Nop>")
