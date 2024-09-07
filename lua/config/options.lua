45a93cb7ec952fe4f78f67ead5a47906d864efb0
ilaumjd <ilaumjd@gmail.com>
2024-09-07 22:30:13 +0700

    setup options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.completeopt = "menu,menuone,noselect"
+opt.expandtab = true -- Use spaces instead of tabs
+opt.ignorecase = true -- Ignore case
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.relativenumber = true -- Relative line numbers
+opt.shiftwidth = 2 -- Size of an indent
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.termguicolors = true -- True color support
+opt.timeoutlen = 300 -- For which-key plugin
+opt.undofile = true
