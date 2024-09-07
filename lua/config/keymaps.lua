f5d69cf6aec16cc900f3138d95521600b3ce9032
ilaumjd <ilaumjd@gmail.com>
2024-09-08 00:27:04 +0700

    configure keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,13 +8,22 @@ local function map(mode, lhs, rhs, opts)
 end
 
 ------ NORMAL ------
-map("n", "x", '"_x', { desc = "Prevent copy on press x" })
+map("n", "x", '"_x', { desc = "Delete character without copying to register" })
+map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+map("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer" })
+map("n", "<S-h>", ":bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+map("n", "<leader>bd", ":bdelete<CR>", { desc = "Close current buffer" })
+map("n", "<leader>bo", "<Cmd>BufferCloseAllButCurrent<CR>", { desc = "Close other buffers" })
 
------ INSERT ------
-map("i", "jk", "<ESC>", { desc = "Press jk fast to enter" })
+------ INSERT ------
+map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 
 ------ VISUAL BLOCK ------
-map("x", "J", ":move '>+1<CR>gv-gv", { desc = "Move text down" })
-map("x", "K", ":move '<-2<CR>gv-gv", { desc = "Move text up" })
-map("x", "<A-j>", ":move '>+1<CR>gv-gv", { desc = "Move text down (Alt)" })
-map("x", "<A-k>", ":move '<-2<CR>gv-gv", { desc = "Move text up (Alt)" })
+map("x", "J", ":move '>+1<CR>gv-gv", { desc = "Move selected text down" })
+map("x", "K", ":move '<-2<CR>gv-gv", { desc = "Move selected text up" })
+map("x", "<A-j>", ":move '>+1<CR>gv-gv", { desc = "Move selected text down (Alt)" })
+map("x", "<A-k>", ":move '<-2<CR>gv-gv", { desc = "Move selected text up (Alt)" })
--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -8,11 +8,5 @@ return {
       animation = true,
     },
     version = '^1.0.0',
-    config = function()
-      vim.keymap.set('n', '<S-h>', '<Cmd>BufferPrevious<CR>', { desc = 'Go to previous tab', noremap = true, silent = true })
-      vim.keymap.set('n', '<S-l>', '<Cmd>BufferNext<CR>', { desc = 'Go to next tab', noremap = true, silent = true })
-      vim.keymap.set('n', '<leader>bo', '<Cmd>BufferCloseAllButCurrent<CR>', { desc = 'Close all tabs but current', noremap = true, silent = true })
-      vim.keymap.set('n', '<leader>bd', '<Cmd>BufferClose<CR>', { desc = 'Close current tab', noremap = true, silent = true })
-    end,
   },
 }
