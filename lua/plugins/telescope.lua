eaed04e0fe2d6cb0353c1773a47579f0ccfe9fba
ilaumjd <ilaumjd@gmail.com>
2024-09-07 22:52:46 +0700

    setup telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,46 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.5',
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
+  config = function()
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
+    local actions = require('telescope.actions')
+    
+    telescope.setup {
       defaults = {
         mappings = {
           i = {
-            ["<C-k>"] = "move_selection_previous",
-            ["<C-j>"] = "move_selection_next",
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
           },
         },
       },
-    },
-  },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      }
+    }
+    
+    -- Load the fzf extension
+    telescope.load_extension('fzf')
+
+    -- Keybindings
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Grep files' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Find keymaps' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Find recent files' })
+  end
 }
