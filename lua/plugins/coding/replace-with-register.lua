556cdaf9c4698d325e49bc66bf97d36640dd3e92
ilaumjd <ilaumjd@gmail.com>
2024-09-08 15:04:54 +0700

    fix replace with register

--- lua/plugins/coding/replace-with-register.lua
+++ lua/plugins/coding/replace-with-register.lua
@@ -1,5 +1,9 @@
 return {
   {
     "vim-scripts/ReplaceWithRegister",
+    keys = {
+      { "gr", "<Plug>ReplaceWithRegisterOperator", desc = "Replace with register operator" },
+      { "grr", "<Plug>ReplaceWithRegisterLine", desc = "Replace with register line" },
+    },
   },
 }
