4eaeed91d30e917433d97f9865fda5cbe7fd70c9
ilaumjd <ilaumjd@gmail.com>
2024-09-08 09:55:51 +0700

    create plugin dirs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,12 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    { import = "plugins.coding" },
+    { import = "plugins.helper" },
+    { import = "plugins.lang" },
+    { import = "plugins.langtools" },
+    { import = "plugins.ui" },
+    { import = "plugins.util" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
