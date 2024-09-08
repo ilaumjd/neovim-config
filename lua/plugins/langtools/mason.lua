78dad30334f40f08cf0c0b80284553e047b2398b
ilaumjd <ilaumjd@gmail.com>
2024-09-08 12:18:22 +0700

    mason ensure_installed

--- lua/plugins/langtools/mason.lua
+++ lua/plugins/langtools/mason.lua
@@ -9,6 +9,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "nil_ls",
+          "rust_analyzer",
+          "tsserver",
+        },
         automatic_installation = true,
       })
     end,
