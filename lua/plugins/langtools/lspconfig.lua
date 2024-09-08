309ecf7591628e65101c25d725032e202cb198b0
ilaumjd <ilaumjd@gmail.com>
2024-09-08 11:47:52 +0700

    extract mason

--- lua/plugins/langtools/lspconfig.lua
+++ lua/plugins/langtools/lspconfig.lua
@@ -1,19 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        automatic_installation = true,
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     config = function(_, opts)
       -- Keymaps
