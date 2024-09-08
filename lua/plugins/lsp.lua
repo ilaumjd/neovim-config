5f2f067f58bde11caa582591e71d7f2227d6dc6c
ilaumjd <ilaumjd@gmail.com>
2024-09-08 07:57:23 +0700

    setup conform

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,15 +3,16 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        -- ensure_installed = { "lua_ls" },
+        automatic_installation = false,
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
@@ -20,19 +21,16 @@ return {
       lspconfig.lua_ls.setup({})
 
       -- Keymaps
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Hover Documentation" })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to Definition" })
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "Find References" })
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to Implementation" })
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename" })
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Action" })
-      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
-      vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Quickfix Diagnostics" })
-
-      -- Format on save
-      vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find References" })
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Quickfix Diagnostics" })
 
       -- Show diagnostic on hover
       vim.api.nvim_create_autocmd("CursorHold", {
@@ -41,14 +39,14 @@ return {
           local opts = {
             focusable = false,
             close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-            border = 'rounded',
-            source = 'always',
-            prefix = ' ',
-            scope = 'cursor',
+            border = "rounded",
+            source = "always",
+            prefix = " ",
+            scope = "cursor",
           }
           vim.diagnostic.open_float(nil, opts)
-        end
+        end,
       })
-    end
-  }
+    end,
+  },
 }
