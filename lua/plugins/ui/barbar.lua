02f9909b27363cfc1a53b263ecbae1d306542d5e
ilaumjd <ilaumjd@gmail.com>
2024-09-08 16:46:49 +0700

    fix barbar + keymaps

--- lua/plugins/ui/barbar.lua
+++ lua/plugins/ui/barbar.lua
@@ -5,8 +5,12 @@ return {
       "lewis6991/gitsigns.nvim",
       "nvim-tree/nvim-web-devicons",
     },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
     opts = {
       animation = true,
+      insert_at_start = true,
     },
     version = "^1.0.0",
   },
