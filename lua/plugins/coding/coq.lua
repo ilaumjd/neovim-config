12f106deeb6fcfac828bd57a90a8c1e7ba060133
ilaumjd <ilaumjd@gmail.com>
2024-09-08 15:05:35 +0700

    refactor coq

--- lua/plugins/coding/coq.lua
+++ lua/plugins/coding/coq.lua
@@ -27,23 +27,4 @@ return {
       },
     }
   end,
-  config = function()
-    local coq = require("coq")
-
-    local lspconfig = require("lspconfig")
-
-    lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities({
-      -- Your Lua LSP settings here
-    }))
-
-    vim.keymap.set("i", "<C-h>", function()
-      return vim.fn.pumvisible() == 1 and "<C-e>" or "<C-h>"
-    end, { expr = true })
-    vim.keymap.set("i", "<C-j>", function()
-      return vim.fn.pumvisible() == 1 and "<C-n>" or "<C-j>"
-    end, { expr = true })
-    vim.keymap.set("i", "<C-k>", function()
-      return vim.fn.pumvisible() == 1 and "<C-p>" or "<C-k>"
-    end, { expr = true })
-  end,
 }
