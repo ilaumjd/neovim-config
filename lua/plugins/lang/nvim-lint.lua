87d9e0bf3743228c23cb25555bb4b97f03a31cd0
ilaumjd <ilaumjd@gmail.com>
2024-09-13 13:30:48 +0700

    ignore lint errors

--- lua/plugins/lang/nvim-lint.lua
+++ lua/plugins/lang/nvim-lint.lua
@@ -25,7 +25,7 @@ return {
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        lint.try_lint(nil, { ignore_errors = true })
       end,
     })
 
