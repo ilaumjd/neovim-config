6cd38d9b03959bbfa640caa17d7e07f3e50346c3
ilaumjd <ilaumjd@gmail.com>
2024-09-13 09:29:03 +0700

    update startup

--- lua/plugins/ui/startup.lua
+++ lua/plugins/ui/startup.lua
@@ -24,19 +24,19 @@ return {
         default_color = "",
         oldfiles_amount = 0,
       },
-      body = {
+      menu = {
         type = "mapping",
         align = "center",
         fold_section = false,
         title = "Basic Commands",
         margin = 5,
         content = {
-          { " Find File", "Telescope find_files", "f" },
-          { " New File", "enew", "n" },
-          { " Recent Files", "Telescope oldfiles", "r" },
-          { " Find Text", "Telescope live_grep", "g" },
-          { " Bookmarks", "Telescope marks", "b" },
-          { " Quit", "qa", "q" },
+          { "󰱼 Find File", "Telescope find_files", "f" },
+          { " New File", "enew", "n" },
+          { " Recent Files", "Telescope oldfiles", "r" },
+          { "󰺮 Find Text", "Telescope live_grep", "g" },
+          { "󰙅 Explorer", "NvimTreeToggle", "e" },
+          { "󰩈 Quit", "qa", "q" },
         },
         highlight = "String",
         default_color = "",
@@ -57,25 +57,7 @@ return {
         default_color = "",
         oldfiles_amount = 0,
       },
-      options = {
-        mapping_keys = true,
-        cursor_column = 0.5,
-        empty_lines_between_mappings = true,
-        disable_statuslines = true,
-        paddings = { 1, 2, 2, 2, 1 },
-      },
-      mappings = {
-        execute_command = "<CR>",
-        open_file = "o",
-        open_file_split = "<c-o>",
-        open_section = "<TAB>",
-        open_help = "?",
-      },
-      colors = {
-        background = "#1f2227",
-        folded_section = "#56b6c2",
-      },
-      parts = { "header", "body", "footer" },
+      parts = { "header", "menu", "footer" },
     })
   end,
 }
