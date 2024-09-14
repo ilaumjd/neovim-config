e1f9ec45047d0f46ae509b42e33e5ddd80af70e4
ilaumjd <ilaumjd@gmail.com>
2024-09-14 15:34:18 +0700

    setup lspsaga

---
+++ lua/plugins/util/lspsaga.lua
@@ -0,0 +1,33 @@
+return {
+  "nvimdev/lspsaga.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+    { "nvim-treesitter/nvim-treesitter" },
+  },
+  event = "LspAttach",
+  config = function()
+    require("lspsaga").setup({})
+
+    local map = vim.keymap.set
+
+    -- Hover
+    map("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover Documentation" })
+    map("n", "J", "<cmd>Lspsaga peek_type_definition<CR>", { desc = "Peek Type Definition" })
+
+    -- Go to
+    map("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Go to Definition" })
+    map("n", "gr", "<cmd>Lspsaga finder<CR>", { desc = "LSP Finder" })
+    map("n", "gy", "<cmd>Lspsaga goto_type_definition<CR>", { desc = "LSP Finder" })
+
+    -- Code
+    map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = "Code Action" })
+    map("n", "<leader>cm", "<cmd>Lspsaga outline<CR>", { desc = "Code Map Outline" })
+    map("n", "<leader>ci", "<cmd>Lspsaga incoming_calls<CR>", { desc = "Incoming Calls" })
+    map("n", "<leader>co", "<cmd>Lspsaga outgoing_calls<CR>", { desc = "Outgoing Calls" })
+
+    -- Diagnostics
+    map("n", "<leader>xd", "<cmd>Lspsaga show_line_diagnostics<CR>", { desc = "Line Diagnostics" })
+    map("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "Previous Diagnostic" })
+    map("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Next Diagnostic" })
+  end,
+}
