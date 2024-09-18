5f8304b62a1270cf82c1da49a2fdce559d69b5ce
ilaumjd <ilaumjd@gmail.com>
2024-09-18 12:09:27 +0700

    setup oil

---
+++ lua/plugins/util/oil.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+    })
+
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil" })
+  end,
+}
