adf262944cad836c3cbffc32dcd2729456e71721
ilaumjd <ilaumjd@gmail.com>
2024-09-19 07:25:14 +0700

    move codeium to ai

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
+    { import = "plugins.ai" },
     { import = "plugins.coding" },
     { import = "plugins.completion" },
     { import = "plugins.helper" },
