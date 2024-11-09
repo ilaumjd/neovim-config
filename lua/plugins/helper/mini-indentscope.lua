59949e5b9a7e9d983d7e2eb5bc4fa86460bf29f2
ilaumjd <ilaumjd@gmail.com>
2024-11-09 08:13:36 +0700

    fix mini-indentscope

--- lua/plugins/helper/mini-indentscope.lua
+++ lua/plugins/helper/mini-indentscope.lua
@@ -2,6 +2,25 @@ return {
   "echasnovski/mini.indentscope",
   version = "*",
   config = function()
-    require("mini.indentscope").setup()
+    require("mini.indentscope").setup({
+      options = {
+        try_as_border = false,
+      },
+      init = function()
+        -- Only enable for normal buffers (excludes special buffers automatically)
+        if vim.bo.buftype ~= "" then
+          vim.b.miniindentscope_disable = true
+        end
+      end,
+    })
+
+    -- For new buffers
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      callback = function()
+        if vim.bo.buftype ~= "" then
+          vim.b.miniindentscope_disable = true
+        end
+      end,
+    })
   end,
 }
