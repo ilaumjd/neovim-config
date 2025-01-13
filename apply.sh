2baa973a16c9d22001d95a82fccea4e729d6d103
ilaumjd <ilaumjd@gmail.com>
2025-01-13 16:18:34 +0700

    create apply.sh script

---
+++ apply.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
+    ln -s "$DOTFILES_DIR/eww" ~/.config/eww
+    ln -s "$DOTFILES_DIR/i3" ~/.config/i3
+    ln -s "$DOTFILES_DIR/picom" ~/.config/picom
+    ln -s "$DOTFILES_DIR/rofi" ~/.config/rofi
+fi
