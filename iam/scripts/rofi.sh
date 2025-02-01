9ae1beb708bb8c7ec4a084e5869d062fcee8a918
ilaumjd <ilaumjd@gmail.com>
2025-02-01 20:47:11 +0700

    fonts: change to inter

--- iam/scripts/rofi.sh
+++ iam/scripts/rofi.sh
@@ -2,17 +2,19 @@
 
 # Function to handle rofi launchers
 rofi_launch() {
-    local mode=$1
-    
-    case "$mode" in
-        "launcher")
-            rofi -show drun
-            ;;
-        "power")
-            rofi -show menu \
-                 -modi "menu:rofi-power-menu --choices=suspend/shutdown/reboot/lockscreen/logout"
-            ;;
-    esac
+  local mode=$1
+
+  case "$mode" in
+  "launcher")
+    rofi -show drun \
+      -font "Inter Bold 16"
+    ;;
+  "power")
+    rofi -show menu \
+      -font "Inter Bold 16" \
+      -modi "menu:rofi-power-menu --choices=suspend/shutdown/reboot/lockscreen/logout"
+    ;;
+  esac
 }
 
 # Call the function with the provided argument
