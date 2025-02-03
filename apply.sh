62468a0ef4ff90222cd87ca80a856fa9c854fb94
ilaumjd <ilaumjd@gmail.com>
2025-02-03 23:15:11 +0700

    niri: add config

--- apply.sh
+++ apply.sh
@@ -3,20 +3,21 @@
 DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
+  ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
-    ln -s "$DOTFILES_DIR/dunst" ~/.config/dunst
-    ln -s "$DOTFILES_DIR/eww" ~/.config/eww
-    ln -s "$DOTFILES_DIR/rofi" ~/.config/rofi
+  ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
+  ln -s "$DOTFILES_DIR/dunst" ~/.config/dunst
+  ln -s "$DOTFILES_DIR/eww" ~/.config/eww
+  ln -s "$DOTFILES_DIR/rofi" ~/.config/rofi
 
-    # x11
-    ln -s "$DOTFILES_DIR/i3" ~/.config/i3
-    ln -s "$DOTFILES_DIR/picom" ~/.config/picom
+  # x11
+  ln -s "$DOTFILES_DIR/i3" ~/.config/i3
+  ln -s "$DOTFILES_DIR/picom" ~/.config/picom
 
-    # wayland
-    ln -s "$DOTFILES_DIR/hypr" ~/.config/hypr
-     
-    # scripts
-    ln -s "$DOTFILES_DIR/iam" ~/.config/iam
+  # wayland
+  ln -s "$DOTFILES_DIR/hypr" ~/.config/hypr
+  ln -s "$DOTFILES_DIR/niri" ~/.config/niri
+
+  # scripts
+  ln -s "$DOTFILES_DIR/iam" ~/.config/iam
 fi
